1174427e4bc1e72edb7f442f01abf55ff5fa6368
tasneem1998 <tasneem.k@homesfy.in>
2022-09-15 20:04:52 +0530

    left sidebar with icon and label

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,60 @@
 import Box from "@mui/material/Box";
+import Switch from "@mui/material/Switch";
 import React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import HomeIcon from '@mui/icons-material/Home';
+import ArticleIcon from '@mui/icons-material/Article';
+import GroupIcon from '@mui/icons-material/Group';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import PersonIcon from '@mui/icons-material/Person';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 
+const sidebar_data = [
+    { icon: <HomeIcon />, label: "Home" },
+    { icon: <ArticleIcon />, label: "Pages" },
+    { icon: <GroupIcon />, label: "Groups" },
+    { icon: <StorefrontIcon />, label: "Marketplace" },
+    { icon: <PersonIcon />, label: "Friends" },
+    { icon: <SettingsIcon />, label: "Settings" },
+    { icon: <AccountBoxIcon />, label: "Profile" },
+    { icon: <DarkModeIcon />, label: <Switch/> },
+]
 const Sidebar = () => {
+
+    const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+    const handleListItemClick = (event, index) => {
+        setSelectedIndex(index);
+    };
+
     return (
         <Box
             p={2}
-            bgcolor="red"
             flex={1}
             sx={{ display: { xs: "none", sm: "block" } }}
         >
-            Sidebar
+            <List component="nav">
+                {sidebar_data.map((item, index) => (
+                    <ListItem disablePadding>
+                        <ListItemButton
+                            selected={selectedIndex === index}
+                            onClick={(event) => handleListItemClick(event, index)}
+                            component="a"
+                        >
+                            <ListItemIcon>
+                                {item.icon}
+                            </ListItemIcon>
+                            <ListItemText primary={item.label} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
         </Box>
     )
 }
